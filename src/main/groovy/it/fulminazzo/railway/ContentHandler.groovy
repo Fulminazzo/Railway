aeea255c50e617f77cc1b9748207af888557744e
Fulminazzo <fulminazzo21@gmail.com>
2024-12-22 00:17:40 +0100

    Added groovydoc

--- src/main/groovy/it/fulminazzo/railway/ContentHandler.groovy
+++ src/main/groovy/it/fulminazzo/railway/ContentHandler.groovy
@@ -2,17 +2,38 @@ package it.fulminazzo.railway
 
 import org.jetbrains.annotations.NotNull
 
+/**
+ * A class responsible for correctly handling paths and returning appropriate files.
+ */
 class ContentHandler {
     static final INDEX_NAME = 'index.html'
 
     final @NotNull File root
 
+    /**
+     * Instantiates a new ContextHandler
+     *
+     * @param rootDirPath the root directory path
+     */
     ContentHandler(@NotNull String rootDirPath) {
         this.root = new File(rootDirPath)
         if (!this.root.isDirectory())
             throw new ContentHandlerException('Could not find directory at path: ' + rootDirPath)
     }
 
+    /**
+     * Resolves the given path by:
+     * <ul>
+     *     <li>if it contains a file extension, it searches for the file in the file system;</li>
+     *     <li>if it does not contain, it searches for the file <code>&lt;path&gt;.html</code> in the file system;</li>
+     *     <li>if it is a directory, it searches for the file <code>&lt;path&gt;/index.html</code> in the file system.</li>
+     * <ul>
+     * In every other case, it throws an exception.
+     *
+     * @param path the path
+     * @return the file
+     * @throws ContentHandlerException the exception thrown in case of error
+     */
     File resolvePath(@NotNull String path) throws ContentHandlerException {
         def file = new File(this.root, path)
         if (file.isFile()) return file
