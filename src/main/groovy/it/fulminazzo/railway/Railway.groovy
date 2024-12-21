449a4ddf89e70269bd7310bdc1d935526b6680ad
Fulminazzo <fulminazzo21@gmail.com>
2024-12-22 00:26:36 +0100

    Created Railway main class

---
+++ src/main/groovy/it/fulminazzo/railway/Railway.groovy
@@ -0,0 +1,18 @@
+package it.fulminazzo.railway
+
+import org.jetbrains.annotations.NotNull
+
+/**
+ * The main root class.
+ */
+class Railway {
+    static final DEFAULT_PORT = 80
+
+    // <port> <rootDir>
+    static void main(String[] args) {
+        def port = DEFAULT_PORT
+        def rootDir = System.getProperty('user.dir')
+
+    }
+    
+}
