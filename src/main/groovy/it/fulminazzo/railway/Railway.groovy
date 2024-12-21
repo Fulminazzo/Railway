3887f24a61335121efa4cdf07296e2cda873f45b
Fulminazzo <fulminazzo21@gmail.com>
2024-12-22 00:27:55 +0100

    Added getPort function and check in main method

--- src/main/groovy/it/fulminazzo/railway/Railway.groovy
+++ src/main/groovy/it/fulminazzo/railway/Railway.groovy
@@ -10,9 +10,31 @@ class Railway {
 
     // <port> <rootDir>
     static void main(String[] args) {
-        def port = DEFAULT_PORT
-        def rootDir = System.getProperty('user.dir')
+        try {
+            def port = DEFAULT_PORT
+            def rootDir = System.getProperty('user.dir')
 
+            if (args.length > 0) port = getPort(args[0])
+        } catch (RailwayException e) {
+            System.err.println(e.message)
+        }
+    }
+
+    /**
+     * Tries to get a numeric port from the given string.
+     * If the string is not a number between 0 and 65535,
+     * a {@link RailwayException} is thrown.
+     *
+     * @param string the string
+     * @return the port
+     */
+    static int getPort(@NotNull String string) throws RailwayException {
+        if (string.isNumber()) {
+            def tmp = string as Integer
+            if (tmp >= 0 && tmp <= 65535)
+                return tmp
+        }
+        throw new RailwayException("Invalid port \"${string}\". A number higher than 0 is required.")
     }
 
     /**
