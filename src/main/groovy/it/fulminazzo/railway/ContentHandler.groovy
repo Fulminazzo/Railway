ec3ca4d3a3fef6ae1e2bde1558254569f1259854
Fulminazzo <fulminazzo21@gmail.com>
2024-12-22 00:13:50 +0100

    Added resolvePath method to resolve given path using set of rules

--- src/main/groovy/it/fulminazzo/railway/ContentHandler.groovy
+++ src/main/groovy/it/fulminazzo/railway/ContentHandler.groovy
@@ -3,6 +3,8 @@ package it.fulminazzo.railway
 import org.jetbrains.annotations.NotNull
 
 class ContentHandler {
+    static final INDEX_NAME = 'index.html'
+
     final @NotNull File root
 
     ContentHandler(@NotNull String rootDirPath) {
@@ -11,4 +13,23 @@ class ContentHandler {
             throw new ContentHandlerException('Could not find directory at path: ' + rootDirPath)
     }
 
+    File resolvePath(@NotNull String path) throws ContentHandlerException {
+        def file = new File(this.root, path)
+        if (file.isFile()) return file
+        else if (file.isDirectory()) {
+            file = new File(file, INDEX_NAME)
+            if (file.isFile()) return file
+            else throw new ContentHandlerException('Could not find path: ' + path)
+        } else {
+            def pattern = ~/(.*)\.([^.]+)/
+            def matcher = path =~ pattern
+            if (matcher.matches())
+                throw new ContentHandlerException('Could not find path: ' + path)
+            else {
+                file = new File(this.root, path + '.html')
+                if (file.isFile()) return file
+                else throw new ContentHandlerException('Could not find path: ' + path)
+            }
+        }
+    }
 }
