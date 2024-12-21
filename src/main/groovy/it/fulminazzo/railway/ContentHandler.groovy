53fd84165e012d802b698dd56d072481c0fb64da
Fulminazzo <fulminazzo21@gmail.com>
2024-12-22 00:07:10 +0100

    Added ContentHandler constructor

--- src/main/groovy/it/fulminazzo/railway/ContentHandler.groovy
+++ src/main/groovy/it/fulminazzo/railway/ContentHandler.groovy
@@ -1,4 +1,14 @@
 package it.fulminazzo.railway
 
+import org.jetbrains.annotations.NotNull
+
 class ContentHandler {
+    final @NotNull File root
+
+    ContentHandler(@NotNull String rootDirPath) {
+        this.root = new File(rootDirPath)
+        if (!this.root.isDirectory())
+            throw new ContentHandlerException('Could not find directory at path: ' + rootDirPath)
+    }
+
 }
