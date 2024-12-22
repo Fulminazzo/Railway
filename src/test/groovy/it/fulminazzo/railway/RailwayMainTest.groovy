510dff6bc6958dd5615af3ed3867b09e1c6798e0
Fulminazzo <fulminazzo21@gmail.com>
2024-12-22 13:19:03 +0100

    Added tests for invalid arguments in main function

--- src/test/groovy/it/fulminazzo/railway/RailwayMainTest.groovy
+++ src/test/groovy/it/fulminazzo/railway/RailwayMainTest.groovy
@@ -17,4 +17,20 @@ class RailwayMainTest extends Specification {
         System.err.println this.err.toString()
     }
 
+    def 'test invalid arguments'() {
+        when:
+        Railway.main(args.toArray(new String[0]))
+
+        then:
+        this.err.toString() == "${expected}\n"
+
+        where:
+        expected || args
+        'Invalid port \"a\". A number between 0 and 65535 is required.' || ['build', 'a']
+        'Invalid port \"-1\". A number between 0 and 65535 is required.' || ['build', '-1']
+        'Invalid port \"65536\". A number between 0 and 65535 is required.' || ['build', '65536']
+        'Invalid natural \"a\". A number higher than 0 is required.' || ['build', '2', 'not_found.html', 'a']
+        'Invalid natural \"-1\". A number higher than 0 is required.' || ['build', '2', 'not_found.html', '-1']
+    }
+
 }
