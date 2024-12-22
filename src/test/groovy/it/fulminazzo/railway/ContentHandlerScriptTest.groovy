67881d736a52c97982ac29f287284617a3d2b316
Fulminazzo <fulminazzo21@gmail.com>
2024-12-22 21:43:35 +0100

    Added tests for script overriding real method

--- src/test/groovy/it/fulminazzo/railway/ContentHandlerScriptTest.groovy
+++ src/test/groovy/it/fulminazzo/railway/ContentHandlerScriptTest.groovy
@@ -1,6 +1,8 @@
 package it.fulminazzo.railway
 
+import com.sun.net.httpserver.Headers
 import com.sun.net.httpserver.HttpExchange
+import org.slf4j.LoggerFactory
 import spock.lang.Specification
 
 class ContentHandlerScriptTest extends Specification {
@@ -26,6 +28,28 @@ class ContentHandlerScriptTest extends Specification {
         path                            || code                 | message                  | body
         '/scripts/index.groovy/Michael' || HTTPCode.OK          | '/scripts/index.groovy'  | 'Hello, Michael!'
         '/scripts/index.groovy/'        || HTTPCode.BAD_REQUEST | '/scripts/index.groovy/' | 'No name provided!'
+    }
+
+    def 'test script does not override real method'() {
+        given:
+        def handler = new ContentHandler(ROOT_DIR, null, LoggerFactory.getLogger(getClass()))
+        def file = new File(ROOT_DIR, 'scripts/index.groovy')
+        def output = new ByteArrayOutputStream()
+        def exchange = Mock(HttpExchange)
+        exchange.requestURI >> new URI('scripts/')
+        exchange.remoteAddress >> InetSocketAddress.createUnresolved('localhost', 1234)
+        exchange.requestMethod >> 'GET'
+        exchange.responseHeaders >> new Headers()
+        exchange.responseBody >> output
+
+        when:
+        def response = handler.runScript(file, exchange)
+        def responseBody = new String(response.body.bytes)
+        handler.handle(exchange)
+
+        then:
+        responseBody == 'No name provided!'
+        output.toString() == 'No name provided!'
     }
 
 }
